13ea3efe3f0e13622b1d9db7440d901c8a54ec5b
mat13 <mathieu.schmitt13@icloud.com>
2024-06-13 16:08:03 +0200

    first commit

---
+++ Data.js
@@ -0,0 +1,7 @@
+const item = [
+    { id: 1, name: 'Pomme', color: 'Rouge', password: '1234'},
+    { id: 2, name: 'Banane', color: 'Jaune' , password: '1234'},
+    { id: 3, name: 'Orange', color: 'Orange' , password: '1234'}
+];
+
+module.exports = item;
